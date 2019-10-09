05fd8a6bba4a7083bd0b2c691dacc0677ae5fa35
Yqoo <yq910526@outlook.com>
2019-10-09 13:52:21 +0800

    ss

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2019-10-09 11:54:22
  * @LastEditors: Yqoo
- * @LastEditTime: 2019-10-09 13:04:28
+ * @LastEditTime: 2019-10-09 13:51:30
  * @Desc: 
  -->
 <template>
     <div>
         index
         <el-button-group>
-            <el-button type='primary' size='mini'>更新了</el-button>
+            <el-button type='primary' size='mini'>更新</el-button>
             <el-button type='success' size='mini'>首页</el-button>
             <el-button type='danger' size='mini'>首页</el-button>
         </el-button-group>
