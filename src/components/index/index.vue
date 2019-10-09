3427d8d89bc27b5acb35c18631b6952ba70dc8da
Yqoo <yq910526@outlook.com>
2019-10-09 14:12:27 +0800

    update

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,16 +1,16 @@
 <!--
  * @Date: 2019-10-09 11:54:22
  * @LastEditors: Yqoo
- * @LastEditTime: 2019-10-09 13:54:19
+ * @LastEditTime: 2019-10-09 14:12:12
  * @Desc: 
  -->
 <template>
     <div>
         index
         <el-button-group>
-            <el-button type='primary' size='mini'>同步更新测试</el-button>
-            <el-button type='success' size='mini'>首页</el-button>
-            <el-button type='danger' size='mini'>首页</el-button>
+            <el-button type='primary' size='mini'>首页</el-button>
+            <el-button type='success' size='mini'>产品介绍</el-button>
+            <el-button type='danger' size='mini'>解决方案</el-button>
         </el-button-group>
     </div>
 </template>
