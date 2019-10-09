3b30e0a31d3493ae7d2e192a0d58d7dbaa6105ef
Yqoo <yq910526@outlook.com>
2019-10-09 12:07:57 +0800

    Update

---
+++ src/components/product/product.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Date: 2019-10-09 12:06:29
+ * @LastEditors: Yqoo
+ * @LastEditTime: 2019-10-09 12:06:29
+ * @Desc: 
+ -->
+<template>
+    <div>
+        product
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'product'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/scheme/scheme.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Date: 2019-10-09 12:06:54
+ * @LastEditors: Yqoo
+ * @LastEditTime: 2019-10-09 12:06:54
+ * @Desc: 
+ -->
+<template>
+    <div>
+        scheme
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'scheme'
+}
+</script>
+
+<style>
+
+</style>
