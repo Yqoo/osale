4ab247d47e76dc25678f404e777dd9dab3fbeae1
Yqoo <yq910526@outlook.com>
2019-10-09 12:33:17 +0800

    UI

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,12 +1,17 @@
 <!--
  * @Date: 2019-10-09 11:54:22
  * @LastEditors: Yqoo
- * @LastEditTime: 2019-10-09 11:54:22
+ * @LastEditTime: 2019-10-09 12:31:15
  * @Desc: 
  -->
 <template>
     <div>
         index
+        <el-button-group>
+            <el-button type='primary' size='mini'>首页</el-button>
+            <el-button type='success' size='mini'>首页</el-button>
+            <el-button type='danger' size='mini'>首页</el-button>
+        </el-button-group>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+/*
+ * @Date: 2019-10-09 11:45:17
+ * @LastEditors: Yqoo
+ * @LastEditTime: 2019-10-09 11:45:17
+ * @Desc: 
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
